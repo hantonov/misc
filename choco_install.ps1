dd4935d9f2edddcf48273b528f4a2f1610aebd64
Hillel Antonov <dev.antonov.hillel@dev.bwater.com>
2019-10-08 08:35:08 -0400

    enable winrm over https

--- choco_install.ps1
+++ choco_install.ps1
@@ -1,10 +1,15 @@
-# installing chocolatey
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName $env:COMPUTERNAME
+New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint –Force
+winrm set winrm/config/service/auth '@{Basic="true"}'
+New-NetFirewallRule -DisplayName "Windows Remote Management (HTTPS-In)" -Name "Windows Remote Management (HTTPS-In)" -Profile Any -LocalPort 5986 -Protocol TCP
 
-# Globally Auto confirm every action
-choco feature enable -n allowGlobalConfirmation
+# # installing chocolatey
+# Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install googlechrome
-choco install wireshark
-choco install fiddler
+# # Globally Auto confirm every action
+# choco feature enable -n allowGlobalConfirmation
+
+# choco install googlechrome
+# choco install wireshark
+# choco install fiddler
 
