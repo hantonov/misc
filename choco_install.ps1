1d6ab79bd339db454a5209328674cd9a23983b04
Hillel Antonov <dev.antonov.hillel@dev.bwater.com>
2019-10-09 12:22:36 -0400

    testing 5

--- choco_install.ps1
+++ choco_install.ps1
@@ -1,7 +1,8 @@
 Write-Host "testing 123"
 $Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName $env:COMPUTERNAME
 New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint –Force
-winrm set winrm/config/service/auth @{Basic="true"}
+$command = 'winrm set winrm/config/service/auth @{Basic="true"}'
+Invoke-Expression $command
 # New-NetFirewallRule -DisplayName "Windows Remote Management (HTTPS-In)" -Name "Windows Remote Management (HTTPS-In)" -Profile Any -LocalPort 5986 -Protocol TCP
 
 # # installing chocolatey
